34adb4bb22ad9996614566e2f6d52b7abd4c6166
Guilherme Haynes <guilherme@haynes.blog.br>
2019-04-05 09:51:21 -0300

    Interface de Acesso a um site HTTP

---
+++ app/http.py
@@ -0,0 +1,10 @@
+import urllib3
+
+
+class Http(object):
+    def __init__(self, url):
+        self.http = urllib3.PoolManager()
+        self.url = url
+
+    def connect(self):
+        return self.http.request('GET', self.url)
